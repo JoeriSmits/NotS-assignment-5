94d4f24f63a21c5cad7a49c0be884cc403e36e90
Joeri Smits <joeri.smits@planet.nl>
2016-03-28 16:49:39 +0200

    Added a UI

---
+++ LoadBalancer/LoadBalancer/Form.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LoadBalancer
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serversLst = new System.Windows.Forms.ListBox();
+            this.serversLst_Label = new System.Windows.Forms.Label();
+            this.LoadBalancer_Label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.port_Label = new System.Windows.Forms.Label();
+            this.startStop_btn = new System.Windows.Forms.Button();
+            this.method_Label = new System.Windows.Forms.Label();
+            this.method_ComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // serversLst
+            // 
+            this.serversLst.FormattingEnabled = true;
+            this.serversLst.ItemHeight = 25;
+            this.serversLst.Location = new System.Drawing.Point(19, 60);
+            this.serversLst.Name = "serversLst";
+            this.serversLst.Size = new System.Drawing.Size(557, 429);
+            this.serversLst.TabIndex = 0;
+            // 
+            // serversLst_Label
+            // 
+            this.serversLst_Label.AutoSize = true;
+            this.serversLst_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.serversLst_Label.Location = new System.Drawing.Point(12, 9);
+            this.serversLst_Label.Name = "serversLst_Label";
+            this.serversLst_Label.Size = new System.Drawing.Size(175, 37);
+            this.serversLst_Label.TabIndex = 1;
+            this.serversLst_Label.Text = "Server Lijst";
+            // 
+            // LoadBalancer_Label
+            // 
+            this.LoadBalancer_Label.AutoSize = true;
+            this.LoadBalancer_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.LoadBalancer_Label.Location = new System.Drawing.Point(808, 9);
+            this.LoadBalancer_Label.Name = "LoadBalancer_Label";
+            this.LoadBalancer_Label.Size = new System.Drawing.Size(224, 37);
+            this.LoadBalancer_Label.TabIndex = 2;
+            this.LoadBalancer_Label.Text = "Load Balancer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(736, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 31);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "8080";
+            // 
+            // port_Label
+            // 
+            this.port_Label.AutoSize = true;
+            this.port_Label.Location = new System.Drawing.Point(643, 63);
+            this.port_Label.Name = "port_Label";
+            this.port_Label.Size = new System.Drawing.Size(51, 25);
+            this.port_Label.TabIndex = 4;
+            this.port_Label.Text = "Port";
+            // 
+            // startStop_btn
+            // 
+            this.startStop_btn.Location = new System.Drawing.Point(736, 106);
+            this.startStop_btn.Name = "startStop_btn";
+            this.startStop_btn.Size = new System.Drawing.Size(296, 62);
+            this.startStop_btn.TabIndex = 5;
+            this.startStop_btn.Text = "Start / Stop LoadBalancer";
+            this.startStop_btn.UseVisualStyleBackColor = true;
+            // 
+            // method_Label
+            // 
+            this.method_Label.AutoSize = true;
+            this.method_Label.Location = new System.Drawing.Point(598, 188);
+            this.method_Label.Name = "method_Label";
+            this.method_Label.Size = new System.Drawing.Size(96, 25);
+            this.method_Label.TabIndex = 6;
+            this.method_Label.Text = "Methode";
+            // 
+            // method_ComboBox
+            // 
+            this.method_ComboBox.FormattingEnabled = true;
+            this.method_ComboBox.Items.AddRange(new object[] {
+            "Session Based",
+            "Cookie Based",
+            "Round Robin",
+            "Load (Requests / Server)"});
+            this.method_ComboBox.Location = new System.Drawing.Point(736, 185);
+            this.method_ComboBox.Name = "method_ComboBox";
+            this.method_ComboBox.Size = new System.Drawing.Size(296, 33);
+            this.method_ComboBox.TabIndex = 7;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 507);
+            this.Controls.Add(this.method_ComboBox);
+            this.Controls.Add(this.method_Label);
+            this.Controls.Add(this.startStop_btn);
+            this.Controls.Add(this.port_Label);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LoadBalancer_Label);
+            this.Controls.Add(this.serversLst_Label);
+            this.Controls.Add(this.serversLst);
+            this.Name = "Form";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox serversLst;
+        private System.Windows.Forms.Label serversLst_Label;
+        private System.Windows.Forms.Label LoadBalancer_Label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label port_Label;
+        private System.Windows.Forms.Button startStop_btn;
+        private System.Windows.Forms.Label method_Label;
+        private System.Windows.Forms.ComboBox method_ComboBox;
+    }
+}
+
